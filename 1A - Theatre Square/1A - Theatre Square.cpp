9b1fb87c62727cb75dc2b9f732c6dea56ae6c411
Rajveer Singh Saggu <rajveersinghsaggu2009@proton.me>
2025-04-22 11:28:15 +0530

    perf: 1A - Theatre Square

--- 1A - Theatre Square/1A - Theatre Square.cpp
+++ 1A - Theatre Square/1A - Theatre Square.cpp
@@ -1,22 +1,16 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    long long n, m, a;
+int main() {
+  long long n, m, a;
 
-    cin >> n >> m >> a;
+  cin >> n >> m >> a;
 
-    if (!(1 <= n <= 1e9) || !(1 <= m <= 1e9) || !(1 <= a <= 1e9))
-    {
-        return -1;
-    }
+  long long x = (n + a - 1) / a;
+  long long y = (m + a - 1) / a;
+  long long z = x * y;
 
-    long long x = (n + a - 1) / a;
-    long long y = (m + a - 1) / a;
-    long long z = x * y;
+  cout << z;
 
-    cout << z;
-
-    return 0;
+  return 0;
 }
